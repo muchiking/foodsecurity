13a026e09c7a7ef0abca23eab988b3e297c3012d
muchiking <joelmuchi@gmail.com>
2024-07-08 12:01:52 +0300

    sssss

--- my_tennis_club/members/migrations/0001_initial.py
+++ my_tennis_club/members/migrations/0001_initial.py
@@ -17,6 +17,8 @@ class Migration(migrations.Migration):
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('firstname', models.CharField(max_length=255)),
                 ('lastname', models.CharField(max_length=255)),
+                ('phone', models.IntegerField(null=True)),
+                ('joined_date', models.DateField(null=True)),
             ],
         ),
     ]
